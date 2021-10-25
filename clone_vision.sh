fdf88abf91ec5f3727f88753fe84dd09fc0ad34a
Polichinel <spm@ifs.ku.dk>
2021-10-25 12:10:53 +0200

    pytorch vision thing

---
+++ clone_vision.sh
@@ -0,0 +1,6 @@
+git clone http://github.com/pytorch/vision.git
+cp vision/references/detection/utils.py ./
+cp vision/references/detection/transforms.py ./
+cp vision/references/detection/coco_eval.py ./
+cp vision/references/detection/engine.py ./
+cp vision/references/detection/coco_utils.py ./
